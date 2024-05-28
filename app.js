b229851e90766220cf972b148fb130d31ff828c0
sufyanmamji8 <sufyanmamji4@gmail.com>
2024-05-28 21:22:37 +0500

    adding more features

--- app.js
+++ app.js
@@ -1,19 +1,69 @@
-let input = document.getElementById('todo')
-let text = document.querySelector('.text')
+// let input = document.getElementById('todo');
+// let text = document.querySelector('.text');
+// let deleteAllButton = document.getElementById('btn2');
 
-function add(){
-    if(input.value === ""){
-        alert('please enter a todo to continue!')
-    }else{
-        let newElement = document.createElement("li")
-        newElement.innerHTML = `${input.value} <i class="fa-solid fa-trash"></i>`;
-        text.appendChild(newElement)
-        input.value = ""
-        newElement.querySelector('i').addEventListener('click' , function remove() {
-            newElement.remove()
-            
-        })
+// function add() {
+//     if (input.value === "") {
+//         alert('please enter a todo to continue!');
+//     } else {
+//         let newElement = document.createElement("li");
+//         newElement.innerHTML = `${input.value} <i class="fa-solid fa-trash"></i> <i class="fa-regular fa-pen-to-square"></i>`;
+//         text.appendChild(newElement);
+//         input.value = "";
+        
+
+//         newElement.querySelector('.fa-trash').addEventListener('click', function () {
+//             newElement.remove();
+
+      
+//         });
+        
+//         newElement.querySelector('.fa-pen-to-square').addEventListener('click', function () {
+//             let edit = prompt('Edit your task or todo:', newElement.firstChild.textContent.trim());
+//             if (edit !== null && edit !== "") {
+//                 newElement.firstChild.textContent = edit + " ";
+       
+    
+        
+//     })
+// }
+
+
+// function deleteAllTodos() {
+//     text.innerHTML = "";
+// }
+
+// deleteAllButton.addEventListener('click', deleteAllTodos);
+
+let input = document.getElementById('todo');
+let text = document.querySelector('.text');
+let deleteAllButton = document.getElementById('btn2');
+
+function add() {
+    if (input.value === "") {
+        alert('please enter a todo to continue!');
+    } else {
+        let newElement = document.createElement("li");
+        newElement.innerHTML = `${input.value} <i class="fa-solid fa-trash"></i> <i class="fa-regular fa-pen-to-square"></i>`;
+        text.appendChild(newElement);
+        input.value = "";
+        
+        newElement.querySelector('.fa-trash').addEventListener('click', function () {
+            newElement.remove();
+        });
+        
+        newElement.querySelector('.fa-pen-to-square').addEventListener('click', function () {
+            let edit = prompt('Edit your task or todo:', newElement.firstChild.textContent.trim());
+            if (edit !== null && edit !== "") {
+                newElement.firstChild.textContent = edit + " ";
+            }
+        });
     }
 }
 
+function deleteAllTodos() {
+    text.innerHTML = "";
+}
+
+deleteAllButton.addEventListener('click', deleteAllTodos);
 
